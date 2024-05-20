7ec7f96c6d82dbd16be42693370105fae06d44ab
Michuaee <michuaepl@gmail.com>
2024-05-20 20:31:11 +0200

    add

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+import tkinter as tk
+def main():
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    root = tk.Tk()
+    root.title("bom_to_ssa")
 
+    data_dict = read_txt_file("indeks_programu.txt")
+    print(write_ssa(data_dict))
+    root.mainloop()
+def read_txt_file(path):
+    # read from txt to a dictionary
+    try:
+        with open(path, 'r') as file:
+            header = file.readline().strip().split(',')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+            data_list = []
 
+            for line in file:
+                values = line.strip().split()
+                entry = dict(zip(header, values))
+                data_list.append(entry)
 
-# Press the green button in the gutter to run the script.
+    except FileNotFoundError:
+        print("No such file or directory")
+    return data_list
+
+def write_ssa(placements):
+    header = '''[VERSION]
+
+[PCB]
+Unit System = MILIMETER
+Coordinate = LOWER LEFT
+Rotation = 0
+Placement Origin = -560.000, 5.000
+Fiducial = CIRCLE, 8.062, -0.839, 544.970, 302.071
+Accept Mark = NONE, 0, 0
+Bad Mark = NONE, 0, 0
+
+[BOARD]
+Board Name = 
+PCB Size = 560.000, 310.400, 3.000
+Array = 1, 10, LOWER RIGHT
+Array Offset = 0.000, 30.000
+
+[PLACEMENTS]
+'''
+
+    placements_str = ''
+    for placement in placements:
+        placements_str += f'"{placement["Ref"]}" {placement["PlacementCentreX"]} {placement["PlacementCentreY"]} 0.000 {placement["Rotation"]} NONE 0 0 0 0 4286 0 "{placement["PartName"]}" "" ""\n'
+
+    with open('indeks_programu.ssa', 'w') as file:
+        file.write(header + placements_str)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
